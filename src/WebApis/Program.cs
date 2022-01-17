1e205c062c7800cb19c2f22b8fcffbe7ee9af5d2
kdcllc <info@kingdavidconsulting.com>
2022-01-17 06:21:53 -0500

    adding validator behavior

--- src/WebApis/Program.cs
+++ src/WebApis/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// add database
+builder.Services.AddDatabase();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
